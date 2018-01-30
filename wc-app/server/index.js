b1b79c92fbfdd1e3f9a03cd6126e634556f5daa6
Kaylin <kaylin.collins68@gmail.com>
2018-01-29 20:13:31 -0800

    fix path to dist

--- wc-app/server/index.js
+++ wc-app/server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/submissions', db.fetchAllForms, (req, res) => {
   res.end();
@@ -21,7 +21,7 @@ app.post('/submit', db.saveForm, (req, res) => {
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 /**
